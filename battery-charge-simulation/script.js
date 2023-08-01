fb86251e4f1250321a87401f3818a1c3e7562f40
leon.kr <leon81.kr@gmail.com>
2023-08-01 18:49:20 +0300

    Upgrade battery to autocharge/discharge

--- battery-charge-simulation/script.js
+++ battery-charge-simulation/script.js
@@ -1,39 +1,54 @@
 // Get the battery level element
-const batteryLevelElement = document.getElementById('batteryLevel');
+const batteryFill = document.getElementById('batteryFill');
 
 // Initial battery charge level (between 0 and 100)
-let batteryChargeLevel = 0;
+let chargeLevel = 0;
+let chargingInterval;
+let dischargingInterval;
 
 // Function to update the battery charge level on the UI
-function updateBatteryLevel() {
-  batteryLevelElement.style.height = `${batteryChargeLevel}%`;
+function updateBatteryFill() {
+  batteryFill.style.height = chargeLevel + '%';
 }
 
-// Function to start charging the battery
 function startCharging() {
-  // Increase the battery charge level by 10% (you can adjust the value as needed)
-  batteryChargeLevel += 10;
-
-  // Ensure the charge level doesn't exceed 100%
-  if (batteryChargeLevel > 100) {
-    batteryChargeLevel = 100;
-  }
-
-  // Update the battery level on the UI
-  updateBatteryLevel();
+  clearInterval(dischargingInterval);
+  chargingInterval = setInterval(() => {
+    if (chargeLevel < 100) {
+      chargeLevel += 10;
+      updateBatteryFill();
+    } else {
+      clearInterval(chargingInterval);
+    }
+  }, 60000); // Charging curve with about 1 minute to charge 50% (10% per 6 seconds)
 }
 
-// Function to stop charging the battery
 function stopCharging() {
-  // Decrease the battery charge level by 5% (you can adjust the value as needed)
-  batteryChargeLevel -= 5;
-
-  // Ensure the charge level doesn't go below 0%
-  if (batteryChargeLevel < 0) {
-    batteryChargeLevel = 0;
-  }
-
-  // Update the battery level on the UI
-  updateBatteryLevel();
+  clearInterval(chargingInterval);
 }
 
+function startDischarging() {
+  clearInterval(chargingInterval);
+  dischargingInterval = setInterval(() => {
+    if (chargeLevel > 0) {
+      chargeLevel -= 10;
+      updateBatteryFill();
+    } else {
+      clearInterval(dischargingInterval);
+    }
+  }, 120000); // Discharging curve with about 2 minutes to discharge 50% (10% per 12 seconds)
+}
+
+function stopDischarging() {
+  clearInterval(dischargingInterval);
+}
+
+function autoCharge() {
+  startCharging();
+  setTimeout(stopCharging, 180000); // AutoCharge for 3 minutes (charge to 100%)
+}
+
+function autoDischarge() {
+  startDischarging();
+  setTimeout(stopDischarging, 180000); // AutoDischarge for 3 minutes (discharge to 0%)
+}
