ef759032a27bd6c6527b348143b3ffb5304b7b3f
leon.kr <leon81.kr@gmail.com>
2023-08-01 19:25:00 +0300

    More refinement

--- battery-charge-simulation/script.js
+++ battery-charge-simulation/script.js
@@ -1,11 +1,14 @@
 // Get the battery level element
 const batteryFill = document.getElementById('batteryFill');
 const batteryLevelText = document.getElementById('batteryLevel');
+const batteryStatusText = document.getElementById('batteryStatus');
 
 // Initial battery charge level (between 0 and 100)
 let chargeLevel = 0;
 let chargingInterval;
 let dischargingInterval;
+let autoChargingTimeout;
+let autoDischargingTimeout;
 
 // Function to update the battery charge level on the UI
 function updateBatteryFill() {
@@ -15,44 +18,54 @@ function updateBatteryFill() {
 
 function startCharging() {
   clearInterval(dischargingInterval);
+  clearInterval(autoDischargingTimeout);
   chargingInterval = setInterval(() => {
     if (chargeLevel < 100) {
       chargeLevel += 5;
       updateBatteryFill();
+      batteryStatusText.textContent = 'Charging';
     } else {
       clearInterval(chargingInterval);
+      batteryStatusText.textContent = 'Stopped';
     }
   }, 10000); // Charging curve with about 10 sec to charge 50%
 }
 
 function stopCharging() {
   clearInterval(chargingInterval);
+  batteryStatusText.textContent = 'Stopped';
 }
 
 function startDischarging() {
   clearInterval(chargingInterval);
+  clearInterval(autoChargingTimeout);
   dischargingInterval = setInterval(() => {
     if (chargeLevel > 0) {
       chargeLevel -= 5;
       updateBatteryFill();
+      batteryStatusText.textContent = 'Discharging';
     } else {
       clearInterval(dischargingInterval);
+      batteryStatusText.textContent = 'Stopped';
     }
-  }, 120000); // Discharging curve with about 20 sec to discharge 50%
+  }, 20000); // Discharging curve with about 20 sec to discharge 50%
 }
 
 function stopDischarging() {
   clearInterval(dischargingInterval);
+  batteryStatusText.textContent = 'Stopped';
 }
 
 function autoCharge() {
   startCharging();
-  setTimeout(stopCharging, 30000); // AutoCharge for 30 sec (charge to 100%)
+  autoChargingTimeout = setTimeout(stopCharging, 30000); // AutoCharge for 30sec (charge to 100%)
+  batteryStatusText.textContent = 'Status: Auto-Charging';
 }
 
 function autoDischarge() {
   startDischarging();
-  setTimeout(stopDischarging, 30000); // AutoDischarge for 30 sec (discharge to 0%)
+  autoDischargingTimeout = setTimeout(stopDischarging, 30000); // AutoDischarge for 30sec (discharge to 0%)
+  batteryStatusText.textContent = 'Status: Auto-Discharging';
 }
 
 function updateBatteryFill() {
